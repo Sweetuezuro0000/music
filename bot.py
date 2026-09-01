593e52f4164047ef550801c7e58abcdacef6ebfc
Sweetuezuro0000 <113238512+Sweetuezuro0000@users.noreply.github.com>
2026-09-01 12:09:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,6 +29,8 @@ from force_sub import (
     subscription_required,
     register_force_sub_handlers
 )
+import os
+from aiohttp import web
 # =========================================================
 # BOT
 # =========================================================
@@ -110,6 +112,29 @@ def register_handlers():
     register_admin_handlers(dp)
     register_security_handlers(dp)
     register_force_sub_handlers(dp)
+
+async def health(request):
+    return web.Response(text="MyBank Bot is running!")
+
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get("/", health)
+
+    port = int(os.environ.get("PORT", "10000"))
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    site = web.TCPSite(
+        runner,
+        "0.0.0.0",
+        port
+    )
+
+    await site.start()
+
+    print(f"🌐 Web server running on port {port}")
 # =========================================================
 # MAIN
 # =========================================================
@@ -123,7 +148,7 @@ async def main():
     register_handlers()
 
     print("🏦 MyBank Bot Started")
-
+    await start_web_server()
     # Start bot
     await dp.start_polling(bot)
 
